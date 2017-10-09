7fbde3eca8944e5a9369aaf5d27d9437ada19718
Greg Toombs <reinderien@users.noreply.github.com>
2017-10-09 15:18:22 -0400

    Add timer graph

--- range-analysis.r
+++ range-analysis.r
@@ -1,13 +1,17 @@
 #!/usr/bin/R -q -f
 
 library(ggplot2)
+library(scales)
+
+
+# Variable setup ###########################################################
 
 # taus = t/RC
 # t = timer*s/f
 
 R = t(c(270, 10e3, 1e6))    # all drive resistors
 colnames(R) = R
-taustable = 6               # taus to stabilization
+taustable = 7               # taus to stabilization
 taufall = -log(1.1/5)       # taus from 5V to 1.1V
 taurise = -log(1-1.1/5)     # taus from 0V to 1.1V     
 f = 16e6                    # timer frequency
@@ -25,14 +29,13 @@ Cmax_time_compare = t(replicate(length(s), as.vector(Cmax_time)))
 
 # Based on the max timer, max caps for each R and s
 Cmax_timer = ((2^16 - 1)*s/f/taustable) %*% (1/R)
-
 Cmax = pmin(Cmax_timer, Cmax_time_compare)
 
 # Based on a timer count of 1, min caps for each R and s
 Cmin = (s/f/taurise) %*% (1/R)
 
 # various E12 capacitors over the full range
-C = matrix(10^seq(-12, -3, by=1/12))
+C = matrix(10^seq(-14, -2, by=1/12))
 rownames(C) = C  # sprintf('%.2e', C)
 
 # The stabilization time is two-dimensional, over C and R.
@@ -41,40 +44,63 @@ tm = taustable*C %*% R
 # The stabilization timer value is three-dimensional, over C, R, and s.
 timer = drop((f*tm) %o% (1/s))
 
-# For every C there is a one-dimensional array of times and a
-# two-dimensional array of timer values.
 
-
+# Plot utilities ###########################################################
 
 # Adapted from https://stackoverflow.com/questions/30179442
-log10_breaks = function(maj) {
+log_breaks = function(maj, radix=10) {
   function(x) {
-    minx         = floor(min(log10(x), na.rm=T)) - 1
-    maxx         = ceiling(max(log10(x), na.rm=T)) + 1
+    minx         = floor(min(logb(x,radix), na.rm=T)) - 1
+    maxx         = ceiling(max(logb(x,radix), na.rm=T)) + 1
     n_major      = maxx - minx + 1
     major_breaks = seq(minx, maxx, by=1)
-    if (maj==TRUE) breaks = major_breaks
-    else breaks = rep(log10(seq(1, 9, by=1)), times=n_major) +
-                  rep(major_breaks, each=9)
-    10^breaks
+    if (maj) {
+      breaks = major_breaks
+    } else {
+      if (radix == 2) {
+        steps = logb(8:15,2) - 3
+      } else steps = logb(1:(radix-1),radix)
+      breaks = rep(steps, times=n_major) +
+               rep(major_breaks, each=radix-1)
+    }
+    radix^breaks
   }
 }
-scale_x_log10_eng = function(...) {
-  scale_x_log10(..., breaks=log10_breaks(TRUE),
-                     minor_breaks=log10_breaks(FALSE))
+scale_x_log_eng = function(..., radix=10) {
+  scale_x_continuous(...,
+                     trans=log_trans(radix),
+                     breaks=log_breaks(TRUE, radix),
+                     minor_breaks=log_breaks(FALSE, radix))
 }
-scale_y_log10_eng = function(...) {
-  scale_y_log10(..., breaks=log10_breaks(TRUE),
-                     minor_breaks=log10_breaks(FALSE))
+scale_y_log_eng = function(..., radix=10) {
+  scale_y_continuous(...,
+                     trans=log_trans(radix),
+                     breaks=log_breaks(TRUE, radix),
+                     minor_breaks=log_breaks(FALSE, radix))
 }
+
+
+# Plotting #################################################################
 
 tm_df = as.data.frame.table(tm)
 colnames(tm_df) = c('C', 'R', 't')
 tm_df$C = as.numeric(levels(tm_df$C))[tm_df$C]
 ggplot(tm_df, aes(x=C, y=t, colour=R, group=R)) +
-   ggtitle(bquote(paste(
-      'Stabilisation time against R and C for ',
-      t/tau == .(taustable)))) +
+   ggtitle(bquote(paste('Stabilisation time against R and C for ',
+                        t/tau == .(taustable)))) +
    geom_line() +
-   scale_x_log10_eng() + scale_y_log10_eng() +
+   scale_x_log_eng() + scale_y_log_eng() +
+   theme(axis.text.x=element_text(angle=90))
+
+
+tmr_df = as.data.frame.table(timer)
+colnames(tmr_df) = c('C', 'R', 's', 'timer')
+tmr_df$C = as.numeric(levels(tmr_df$C))[tmr_df$C]
+ggplot(tmr_df, aes(x=C, y=timer, colour=R,
+                   linetype=s, group=interaction(R,s))) +
+   ggtitle(bquote(paste('Timer against R and C for ',
+                        t/tau == .(taustable)))) +
+   geom_line() +
+   scale_x_log_eng() +
+   scale_y_log_eng(radix=2, limits=2^c(-1,17)) +
    theme(axis.text.x=element_text(angle=90))
